1dbfc45191c91f0fc955e6ad0ac9c92bfcf5d9c1
Huseyin Kutlukkaya <huseyinkutlukkaya@gmail.com>
2019-02-21 23:22:48 +0300

    Proje Eklendi

---
+++ SihirliKareler/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace SihirliKareler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_ciz = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.WhiteSmoke;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.Silver;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.DimGray;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.Honeydew;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.Location = new System.Drawing.Point(41, 37);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.Size = new System.Drawing.Size(712, 489);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // btn_ciz
+            // 
+            this.btn_ciz.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_ciz.Location = new System.Drawing.Point(1037, 56);
+            this.btn_ciz.Name = "btn_ciz";
+            this.btn_ciz.Size = new System.Drawing.Size(103, 46);
+            this.btn_ciz.TabIndex = 1;
+            this.btn_ciz.Text = "ÇİZ";
+            this.btn_ciz.UseVisualStyleBackColor = true;
+            this.btn_ciz.Click += new System.EventHandler(this.btn_ciz_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(920, 64);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 30);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(785, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "UZUNLUK :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1162, 590);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_ciz);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Sihirli Kareler";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_ciz;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SihirliKareler/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SihirliKareler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int n=18;
+        int k;
+        int x, y;
+        int tempx, tempy;
+        Color color;
+        void Ciz()
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+            for (int i = 0; i < n; i++)// sutunlar
+            {
+                dataGridView1.Columns.Add("", "");
+                dataGridView1.Columns[i].Width = 35;
+
+            }
+            for (int i = 0; i < n; i++)//satirlar
+            {
+                dataGridView1.Rows.Add();
+            }
+            color = dataGridView1.Rows[0].Cells[0].Style.BackColor;
+            dataGridView1.Columns.Add("", "");//toplamlar için
+            dataGridView1.Columns[n].DefaultCellStyle.BackColor = Color.Red;
+            dataGridView1.Columns[n].Width = 60;
+            dataGridView1.Rows.Add();// toplamlar için
+            dataGridView1.Rows[n].DefaultCellStyle.BackColor = Color.Red;
+        }
+        void N()//N
+        {
+            x = n - 1;
+            y = (n - 1) / 2;
+            dataGridView1.Rows[y].Cells[x].Value = "1";
+            for (int i = 2; i <= n*n; i++)
+            {
+                x++;
+                y++;
+                if (x == n)
+                    x = x % n;
+                if (y == n)
+                    y = y % n;
+                if (dataGridView1.Rows[y].Cells[x].Value != null)
+                {
+                    x = tempx;
+                    y = tempy;
+                    if (x == 0)
+                        x = n - 1;
+                    else
+                        x--;
+
+                    if (x == n )
+                        x= x % n;
+                    
+
+                }
+                dataGridView1.Rows[y].Cells[x].Value = i.ToString();
+
+                tempx = x;
+                tempy = y;
+            }
+        }
+        void N4()//4N
+        {
+            x = 0;
+            y = 0;
+            for (int i = 0; i < n*n; i++)
+            {
+
+                if ( ( ( n / 4 > x || x > (n-n/4)-1)&&(n / 4 > y || y > (n - n / 4) - 1) ) || ((n / 4 <= x && x <= (n - n / 4) - 1) && (n / 4 <= y && y <= (n - n / 4) - 1))  )
+                    dataGridView1.Rows[y].Cells[x].Value = ((x + 1) + (y ) * n).ToString();
+              else
+                    dataGridView1.Rows[y].Cells[x].Value = (n-x + (n-1-y) *n).ToString();
+
+
+                x++;
+                if (x == n || x < 0)
+                {
+                    x = x % n;
+                    y++;
+                }
+               
+            }
+        }
+        void N4P1()//4N+1
+        {
+            k = n / 2;
+            x = k - 1;
+            y = (k - 1) / 2;
+            dataGridView1.Rows[y].Cells[x].Value = "1";//A
+            dataGridView1.Rows[y+k].Cells[x+k].Value = 1 + k*k ;//B
+            dataGridView1.Rows[y ].Cells[x + k].Value = 1+2*k * k;//C
+            dataGridView1.Rows[y+k].Cells[x ].Value = 1+3*k * k;//D
+
+            for (int i = 2; i <= k*k; i++)
+            {
+                x++;
+                y++;
+                if (x == k)
+                    x = x % k;
+                if (y == k)
+                    y = y % k;
+                if (dataGridView1.Rows[y].Cells[x].Value != null)
+                {
+              
+                    x = tempx;
+                    y = tempy;
+                    if (x == 0)
+                        x = k- 1;
+                    else
+                        x--;
+                    if (x == k )
+                        x= x % k;                  
+
+                }
+                dataGridView1.Rows[y].Cells[x].Value = i.ToString();//A
+                dataGridView1.Rows[y + k].Cells[x + k].Value = (i + k * k).ToString();//B
+                dataGridView1.Rows[y].Cells[x + k].Value = (i + k * 2 * k).ToString();//C
+                dataGridView1.Rows[y + k].Cells[x].Value = (i + k * 3 * k).ToString();//D
+                tempx = x;
+                tempy = y;
+            }
+            int temp;
+            for (int a = 0; a < (n-2)/4;a++)
+            {
+            for (int i = 0; i < k; i++)
+                 {
+                
+                //A VE D ARASIDNA DEĞİŞİM
+                temp = Convert.ToInt32(dataGridView1.Rows[i].Cells[a].Value.ToString());
+                dataGridView1.Rows[i].Cells[a].Value = dataGridView1.Rows[i + k].Cells[a].Value;
+                dataGridView1.Rows[i + k].Cells[a].Value = temp.ToString();
+              
+                    if(a<(n-2)/4-1)// B VE C ARASINDA DEĞİŞİM
+                    {
+                        temp = Convert.ToInt32(dataGridView1.Rows[i + k].Cells[n - 1 - a].Value.ToString());
+                        dataGridView1.Rows[i + k].Cells[n - 1 - a].Value = dataGridView1.Rows[i].Cells[n - 1 - a].Value;
+                        dataGridView1.Rows[i].Cells[n - 1 - a].Value = temp.ToString();
+                    }
+            
+                  
+              
+                }
+            }
+            //orta ve 1. sutun a ve d arası değişim
+            temp = Convert.ToInt32(dataGridView1.Rows[(k-1)/2].Cells[(k - 1) / 2].Value.ToString());
+            dataGridView1.Rows[(k - 1) / 2].Cells[(k - 1) / 2].Value = dataGridView1.Rows[(k - 1) / 2 + k].Cells[(k - 1) / 2].Value;
+            dataGridView1.Rows[(k - 1) / 2 + k].Cells[(k - 1) / 2].Value = temp.ToString();
+
+            temp = Convert.ToInt32(dataGridView1.Rows[(k - 1) / 2].Cells[0].Value.ToString());
+            dataGridView1.Rows[(k - 1) / 2].Cells[0].Value = dataGridView1.Rows[(k - 1) / 2 + k].Cells[0].Value;
+            dataGridView1.Rows[(k - 1) / 2 + k].Cells[0].Value = temp.ToString();
+         
+        }
+       void Topla()
+        {
+            int toplam=0;   
+            for (int b = 0; b < n; b++)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    toplam += Convert.ToInt32(dataGridView1.Rows[b].Cells[i].Value.ToString());
+                }
+                dataGridView1.Rows[b].Cells[n].Value = toplam.ToString();
+                toplam = 0;
+            }
+            for (int b = 0; b < n; b++)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    toplam += Convert.ToInt32(dataGridView1.Rows[i].Cells[b].Value.ToString());
+                }
+                dataGridView1.Rows[n].Cells[b].Value = toplam.ToString();
+                toplam = 0;
+            }
+            for (int b = 0; b < n; b++)
+            {
+                toplam += Convert.ToInt32(dataGridView1.Rows[b].Cells[b].Value.ToString());
+
+            }
+            dataGridView1.Rows[n].Cells[n].Value = toplam.ToString();
+
+        }
+      
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+        
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_ciz_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                n = Convert.ToInt32(textBox1.Text);
+                if (n<3)
+                {
+                    MessageBox.Show("En küçük değer 3 giriniz.");
+                    return;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Sadece 2 den büyük sayı giriniz.");
+                return;
+            }
+
+            Ciz();
+          
+         
+           if (n % 2 == 1)
+            {
+                N();
+            }
+            else if ((n/2) % 2 == 0 )
+            {
+                N4();
+               
+            }
+            else if ((n / 2) % 2 == 1)
+            {
+                N4P1();
+            }
+            Topla();
+        }
+    }
+}
